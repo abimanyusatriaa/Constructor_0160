19a97a18c4771fc602f4447e151c85879c965ad7
abimanyusatriaa <adji3890@gmail.com>
2024-05-14 11:02:20 +0700

    void mahasiswa int main

--- Static Function/Static Function.cpp
+++ Static Function/Static Function.cpp
@@ -12,11 +12,36 @@ public:
     void setID();
     void printALL();
 
-    
+    static void setNim(int pNim) { nim = pNim; }
+    static int getNim() { return nim; }
+
+    mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int mahasiswa::nim = 0;
+
+void mahasiswa::printALL() {
+    cout << "ID   = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
 }
+
+int main() {
+    mahasiswa mhs1("Sri Dadi");
+    mahasiswa mhs2("Budi Jatmiko");
+    mahasiswa::setNim(9);
+    mahasiswa mhs3("Andi Janu");
+    mahasiswa mhs4("Joko Wahono");
+
+    mhs1.printALL();
+    mhs2.printALL();
+    mhs3.printALL();
+    mhs4.printALL();
+
+    cout << "akses dari luar objek = " << mahasiswa::getNim() << endl;
+    system("pause");
+
+    return 0;
+}
+
 
