d5b4ddc3798290501ba6e6a57776dad98c3c5f5c
abimanyusatriaa <adji3890@gmail.com>
2024-05-14 10:15:11 +0700

    constructor class static

--- CONSTRUCTOR/CONSTRUCTOR.cpp
+++ CONSTRUCTOR/CONSTRUCTOR.cpp
@@ -2,6 +2,20 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa {
+public:
+    static int nim;
+    int id;
+    string nama;
+
+    void setID();
+    void printALL();
+
+    Mahasiswa(string pnama) :nama(pnama) {
+        setID();
+    }
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
