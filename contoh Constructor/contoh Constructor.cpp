4a0baa1ee00d259cc24e3963db9d7f867676fa32
abimanyusatriaa <adji3890@gmail.com>
2024-05-14 11:14:25 +0700

    membuat 4 constructor void mahasiswa

--- contoh Constructor/contoh Constructor.cpp
+++ contoh Constructor/contoh Constructor.cpp
@@ -21,7 +21,22 @@ mahasiswa::mahasiswa() {
 }
 
 mahasiswa::mahasiswa(int iNim) {
+    nim = iNim;
+}
 
+mahasiswa::mahasiswa(string iNama) {
+    nim = 0;
+    nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+    nim = iNim;
+    nama = iNama;
+}
+
+void mahasiswa::cetak() {
+    cout << endl << "Nim =" << nim << endl;
+    cout << " Nama = " << nama << endl;
 }
 
 int main()
