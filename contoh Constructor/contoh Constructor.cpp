7c19480b5960a5f1045ff0d68d6f68fdfb17499f
abimanyusatriaa <adji3890@gmail.com>
2024-05-14 11:08:33 +0700

    contoh constructor class mahasiswa

---
+++ contoh Constructor/contoh Constructor.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+    int nim;
+    string nama;
+public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
+
+mahasiswa::mahasiswa() {
+    nim = 0;
+    nama = "";
+}
+
+mahasiswa::mahasiswa(int iNim) {
+
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
