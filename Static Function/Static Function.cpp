8f449e50e3360b55827754eeda9a30d3fd184425
abimanyusatriaa <adji3890@gmail.com>
2024-05-14 10:41:07 +0700

    static function

---
+++ Static Function/Static Function.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printALL();
+
+    
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
